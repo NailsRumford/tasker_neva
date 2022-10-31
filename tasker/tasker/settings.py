8e08a3ec685860c255b567aa146e6505bc1a4181
Волков Артем <x514@list.ru>
2022-10-31 19:23:03 +0300

    исправил

--- tasker/tasker/settings.py
+++ tasker/tasker/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ObjectOfProtection.apps.ObjectOfProtectionConfig'
+    'object_of_protection.apps.ObjectOfProtectionConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
