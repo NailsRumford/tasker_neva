e30eee05e26fd707cb400deefb2967b062964482
Волков Артем <x514@list.ru>
2022-11-29 21:26:14 +0300

    123131

--- tasker/users/forms.py
+++ tasker/users/forms.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
-from users.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
 class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'rank') 
+        fields = ('first_name', 'last_name', 'username', 'email', 'rank')
--- tasker/users/models.py
+++ tasker/users/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-RANK =(('A','Админ базы данных'),
-       ('E','Инженер'),
-       ('R','Техник'),
-       ('S','Обследовальщик'),
-       ('F','Монтажник'))
-
 
 class User(AbstractUser):
-    rank = models.CharField(max_length=1, choices=RANK)
-
+    RANK_PERSON = (('A', 'Админ базы данных'),
+            ('E', 'Инженер'),
+            ('R', 'Техник'),
+            ('S', 'Обследовальщик'),
+            ('F', 'Монтажник'))
+    rank = models.CharField(max_length=1, choices=RANK_PERSON )
