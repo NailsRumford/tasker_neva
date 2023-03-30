b282b2828198baeed31da38bf809e7c11a906a08
Артем Волков <NailsRumford@internet.ru>
2023-03-30 23:53:02 +0300

    Красота

--- tasker/core/context_processors/year.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def year(request):
-    """Возвращает переменную с текущим годом."""
-    year = int(datetime.now().year)
-    return {
-        "year": year
-    }
--- tasker/core/templatetags/user_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def addclass(field, css):
-    """Добавляет в field  атрибут css"""
-    return field.as_widget(attrs={'class': css})
